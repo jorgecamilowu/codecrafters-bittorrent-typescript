b44072c7caca4af2eaee064d6c099785666f2182
jorgewu <jorge.wu@optilyz.com>
2024-12-21 07:30:05 +0100

    decode beenecoded integers

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,15 @@
 // Examples:
 // - decodeBencode("5:hello") -> "hello"
 // - decodeBencode("10:hello12345") -> "hello12345"
+
+function validateBeencodedInt(beencoded: string): boolean {
+  return (
+    beencoded.length >= 3 &&
+    beencoded[0] === "i" &&
+    beencoded[bencodedValue.length - 1] === "e"
+  );
+}
+
 function decodeBencode(bencodedValue: string): string {
   /* This function is used to decode a bencoded string
     The bencoded string is a string that is prefixed by the length of the string
@@ -13,9 +22,19 @@ function decodeBencode(bencodedValue: string): string {
       throw new Error("Invalid encoded value");
     }
     return bencodedValue.substring(firstColonIndex + 1);
-  } else {
-    throw new Error("Only strings are supported at the moment");
   }
+
+  if (bencodedValue[0] === "i") {
+    if (!validateBeencodedInt(bencodedValue)) {
+      throw new Error(
+        "Invalid beencoded integer format. Should start with 'i' and end with 'e'"
+      );
+    }
+
+    return bencodedValue.substring(1, bencodedValue.length - 1);
+  }
+
+  throw new Error("Unsupported encoded value");
 }
 
 const args = process.argv;
