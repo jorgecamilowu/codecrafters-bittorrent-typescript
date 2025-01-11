7cb47e5c55e2492c8c79462cbc409b23d57703e4
jorgewu <jorge.wu@optilyz.com>
2025-01-11 08:21:44 +0100

    fix message buffer test

--- app/peer/MessageBuffer.test.ts
+++ app/peer/MessageBuffer.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect, mock } from "bun:test";
 import { MessageBuffer } from "./MessageBuffer";
+import { Message } from "./Message";
 
 describe("Message Buffer", () => {
   const onComplete = mock();
@@ -12,16 +13,16 @@ describe("Message Buffer", () => {
     messageBuffer.receive(Buffer.from([10, 0, 0, 0, 4, 1, 2]), {} as any);
     messageBuffer.receive(Buffer.from([3, 4]), {} as any);
 
-    const firstBuffer = Buffer.from([
-      0, 0, 0, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    ]);
+    const firstMessage = Message.decode(
+      Buffer.from([0, 0, 0, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    );
 
-    const secondBuffer = Buffer.from([0, 0, 0, 4, 1, 2, 3, 4]);
+    const secondBuffer = Message.decode(Buffer.from([0, 0, 0, 4, 1, 2, 3, 4]));
 
     const calledFirstBuffer = onComplete.mock.calls[0][0];
-    expect([...firstBuffer]).toEqual([...calledFirstBuffer]);
+    expect(firstMessage).toEqual(calledFirstBuffer);
 
     const calledSecondBuffer = onComplete.mock.calls[1][0];
-    expect([...secondBuffer]).toEqual([...calledSecondBuffer]);
+    expect(secondBuffer).toEqual(calledSecondBuffer);
   });
 });
